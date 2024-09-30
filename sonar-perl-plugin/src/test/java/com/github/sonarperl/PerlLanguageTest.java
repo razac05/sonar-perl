a8a09ce802d04053ca1b24f480a50723f51efc5d
Ron Behlmann <rdbehlmann@gmail.com>
2024-09-30 09:58:51 -0400

    Update PerlLanguageTest.java

--- sonar-perl-plugin/src/test/java/com/github/sonarperl/PerlLanguageTest.java
+++ sonar-perl-plugin/src/test/java/com/github/sonarperl/PerlLanguageTest.java
@@ -1,7 +1,7 @@
 package com.github.sonarperl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import java.util.List;
 import org.junit.Test;
 import org.sonar.api.config.internal.MapSettings;
 
@@ -10,7 +10,7 @@ public class PerlLanguageTest {
     @Test
     public void testDefaults() {
         PerlLanguage lang = new PerlLanguage(new MapSettings().asConfig());
-        assertThat(lang.getFileSuffixes()).isEqualTo(new String[] {".pl", ".pm", ".t"});
+        assertThat(lang.getFileSuffixes()).isEqualTo(new List<String> {".pl", ".pm", ".t"});
         assertThat(lang.hasValidSuffixes("my.file")).isFalse();
         assertThat(lang.hasValidSuffixes("my.pm")).isTrue();
     }
@@ -18,9 +18,9 @@ public class PerlLanguageTest {
     @Test
     public void testCustomSettings() {
         MapSettings settings = new MapSettings();
-        settings.setProperty(PerlLanguage.FILE_SUFFIXES_KEY, new String[] {"file","","other"});
+        settings.setProperty(PerlLanguage.FILE_SUFFIXES_KEY, new List<String> {"file","","other"});
         PerlLanguage lang = new PerlLanguage(settings.asConfig());
-        assertThat(lang.getFileSuffixes()).isEqualTo(new String[] {"file","other"});
+        assertThat(lang.getFileSuffixes()).isEqualTo(new List<String> {"file","other"});
         assertThat(lang.hasValidSuffixes("my.file")).isTrue();
     }
 
